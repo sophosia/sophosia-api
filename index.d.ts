b5e82bd3b9eda82253b544f41e157863171eaef0
HuntFeng <spacecurvature@gmail.com>
2024-03-14 19:01:48 -0600

    update

--- index.d.ts
+++ index.d.ts
@@ -1,85 +1,82 @@
-export enum Component {
-  Ribbon = "Ribbon",
-  LeftMenu = "LeftMenu",
-  PDFMenu = "PDFMenu",
-  PluginPage = "PluginPage",
+declare enum Component {
+    Ribbon = "Ribbon",
+    LeftMenu = "LeftMenu",
+    PDFMenu = "PDFMenu",
+    PluginPage = "PluginPage"
+}
+interface Button {
+    id: string;
+    icon: string;
+    tooltip: string;
+    component: Component;
+    click: () => void;
+}
+interface View {
+    id: string;
+    component: Component;
+    mount: (container: HTMLElement) => void;
+}
+declare enum PageType {
+    LibraryPage = "LibraryPage",
+    ReaderPage = "ReaderPage",
+    NotePage = "NotePage",
+    ExcalidrawPage = "ExcalidrawPage",
+    HelpPage = "HelpPage",
+    SettingsPage = "SettingsPage",
+    PluginPage = "PluginPage"
+}
+interface Page {
+    id: string;
+    type: PageType;
+    label: string;
+}
+declare class Plugin {
+    /**
+     * Layout controls
+     */
+    /**
+     * Toggle LeftMenu
+     * If visible is given, set the state as it is
+     * @param visible
+     */
+    toggleLeftMenu: (visible?: boolean) => {};
+    /**
+     * Set the view to be shown in LeftMenu
+     * @param viewId - the id of the view to be shown
+     */
+    setLeftMenuView: (viewId: string) => {};
+    /**
+     * Opens a page
+     * @param - the page to be opened
+     */
+    openPage: (page: Page) => {};
+    /**
+     * Add a button
+     * @param button - button to be added
+     */
+    addButton(button: Button): void;
+    /**
+     * Add a view
+     * @param view - must implement the mount function
+     */
+    addView(view: View): void;
+    /**
+     * This function will be executed when plugin is enabled
+     */
+    enable(): Promise<void>;
+    /**
+     * This function will be executed when plugin is disabled
+     */
+    disable(): Promise<void>;
+    /**
+     * Loads data.json in the plugin folder
+     */
+    loadData(): Promise<void>;
+    /**
+     * Save data to data.json in the plugin folder
+     * @param data - must be serializable
+     */
+    saveData(data: any): Promise<void>;
 }
 
-export interface Button {
-  id: string;
-  icon: string;
-  tooltip: string;
-  component: Component;
-  click: () => void;
-}
-
-export interface View {
-  id: string;
-  component: Component;
-  mount: (container: HTMLElement) => void;
-}
-
-export enum PageType {
-  LibraryPage = "LibraryPage",
-  ReaderPage = "ReaderPage",
-  NotePage = "NotePage",
-  ExcalidrawPage = "ExcalidrawPage",
-  HelpPage = "HelpPage",
-  SettingsPage = "SettingsPage",
-  PluginPage = "PluginPage",
-}
-
-export interface Page {
-  id: string;
-  type: PageType;
-  label: string;
-}
-
-export class Plugin {
-  /**
-   * Layout controls
-   */
-  /**
-   * Toggle LeftMenu
-   * If visible is given, set the state as it is
-   * @param visible
-   */
-  toggleLeftMenu: (visible?: boolean) => {};
-  /**
-   * Set the view to be shown in LeftMenu
-   * @param viewId - the id of the view to be shown
-   */
-  setLeftMenuView: (viewId: string) => {};
-  /**
-   * Opens a page
-   * @param - the page to be opened
-   */
-  openPage: (page: Page) => {};
-  /**
-   * Add a button
-   * @param button - button to be added
-   */
-  addButton(button: Button): void;
-  /**
-   * Add a view
-   * @param view - must implement the mount function
-   */
-  addView(view: View): void;
-  /**
-   * This function will be executed when plugin is enabled
-   */
-  enable(): Promise<void>;
-  /**
-   * This function will be executed when plugin is disabled
-   */
-  disable(): Promise<void>;
-  /**
-   * Loads data.json in the plugin folder
-   */
-  loadData(): Promise<void>;
-  /**
-   * Save data to data.json in the plugin folder
-   * @param data - must be serializable
-   */
-  saveData(data: any): Promise<void>;
-}
+export { type Button, Component, type Page, PageType, Plugin, type View };
---
+++ index.ts
@@ -0,0 +1,85 @@
+export enum Component {
+  Ribbon = "Ribbon",
+  LeftMenu = "LeftMenu",
+  PDFMenu = "PDFMenu",
+  PluginPage = "PluginPage",
+}
+
+export interface Button {
+  id: string;
+  icon: string;
+  tooltip: string;
+  component: Component;
+  click: () => void;
+}
+
+export interface View {
+  id: string;
+  component: Component;
+  mount: (container: HTMLElement) => void;
+}
+
+export enum PageType {
+  LibraryPage = "LibraryPage",
+  ReaderPage = "ReaderPage",
+  NotePage = "NotePage",
+  ExcalidrawPage = "ExcalidrawPage",
+  HelpPage = "HelpPage",
+  SettingsPage = "SettingsPage",
+  PluginPage = "PluginPage",
+}
+
+export interface Page {
+  id: string;
+  type: PageType;
+  label: string;
+}
+
+export class Plugin {
+  /**
+   * Layout controls
+   */
+  /**
+   * Toggle LeftMenu
+   * If visible is given, set the state as it is
+   * @param visible
+   */
+  toggleLeftMenu: (visible?: boolean) => {};
+  /**
+   * Set the view to be shown in LeftMenu
+   * @param viewId - the id of the view to be shown
+   */
+  setLeftMenuView: (viewId: string) => {};
+  /**
+   * Opens a page
+   * @param - the page to be opened
+   */
+  openPage: (page: Page) => {};
+  /**
+   * Add a button
+   * @param button - button to be added
+   */
+  addButton(button: Button): void {}
+  /**
+   * Add a view
+   * @param view - must implement the mount function
+   */
+  addView(view: View): void {}
+  /**
+   * This function will be executed when plugin is enabled
+   */
+  async enable(): Promise<void> {}
+  /**
+   * This function will be executed when plugin is disabled
+   */
+  async disable(): Promise<void> {}
+  /**
+   * Loads data.json in the plugin folder
+   */
+  async loadData(): Promise<void> {}
+  /**
+   * Save data to data.json in the plugin folder
+   * @param data - must be serializable
+   */
+  async saveData(data: any): Promise<void> {}
+}
